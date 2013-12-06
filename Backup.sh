099a418e4b52c658ef1125cf4c2b922c7ccc01d9
JackDrogon <jack.xsuperman@gmail.com>
2013-12-06 18:32:49 +0800

    Change Backup Script!

--- Backup.sh
+++ Backup.sh
@@ -1,13 +1,13 @@
 # /usr/bin/env bash
 
-echo $PWD
 BACKUP="$HOME/Program/GitHub/Configure"
 
 VIM="$BACKUP/Vim"
-mkdir $VIM
+mkdir -p $VIM
 cp $HOME/.vimrc $VIM/
 cp $HOME/.gvimrc $VIM/
 cp -R $HOME/.vim $VIM/
 rm ${VIM}.tgz
-tar zcf ${VIM}.tgz ${VIM}
+cd $VIM/..
+tar zcf ${VIM}.tgz Vim
 rm -rf $VIM
